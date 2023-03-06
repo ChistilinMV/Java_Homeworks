496bf890120ea6a86d94295bef68b4bb91e09ff5
MegaLitr <max.chistilin@gmail.com>
2023-03-07 01:50:48 +0300

    Solved tasks of 1'st seminar

--- src/main/java/Homework01.java
+++ src/main/java/Homework01.java
@@ -4,15 +4,43 @@ public static void main(String[] args) {
     System.out.println(isSumBetween10And20(5, 15)); // true
     System.out.println(isSumBetween10And20(7, 15)); // false
 
+    System.out.println();
     System.out.println(isPositive(5)); // true
     System.out.println(isPositive(-3)); // false
 
+    System.out.println();
     printString("abcd", 5); // abcdabcdabcdabcdabcd
 
+    System.out.println();
+    System.out.println(isLeapYear(1988));
+    System.out.println(isLeapYear(1700));
+    System.out.println(isLeapYear(1600));
+
+    System.out.println();
+    printArray(createArray(10, 7));
+
+    System.out.println();
+    zeroOne();
+
+    System.out.println();
+    sixx2();
+
     int[][] array = new int[4][4]; // [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, x], [0, 0, 0, 1]]
-    int x = array[2][3]; 
+    int x = array[2][3]; // не понял зачем нам этот элимент
 
+    diagonal(array);
+}
+private static void printArray(int[] inArray){
+        for (int y:inArray) System.out.print(y + " ");System.out.println();
+}
 
+private static void print2dArray(int[][] in2dArray){
+    for (int[] x1:in2dArray){
+        for (int x2:x1){
+            System.out.print(x2 + " ");
+        }
+        System.out.println();
+    }
 }
 
 /**
@@ -21,26 +49,36 @@ public static void main(String[] args) {
  */
 private static boolean isSumBetween10And20(int a, int b) {
     // проверить, что сумма a и b лежит между 10 и 20
-    return false;
+    int sum = a + b;
+    return sum >= 10 & sum <= 20;
 }
 
 private static boolean isPositive(int x) {
     // проверить, что х положительное
-    return false;
+    return x > 0;
 }
 
 private static void printString(String source, int repeat) {
     // напечатать строку source repeat раз
+    for (int i = 0; i < repeat; i++) {
+        System.out.print(source);
+    }
+    System.out.println();
 }
 
 private static boolean isLeapYear(int year) {
     // проверить, является ли год високосным. если да - return true
-    return false;
+    return (year % 4 == 0) && (year % 100 != 0) | (year % 400 == 0);
 }
 
 private static int[] createArray(int len, int initalValue) {
     // должен вернуть массив длины len, каждое значение которого равно initialValue
-    return null;
+    int[] array1 = new int[len];
+    for (int i = 0; i < len; i++) {
+        array1[i] = initalValue;
+    }
+    return array1;
+    //return null;
 }
 
 /**
@@ -52,4 +90,31 @@ private static int[] createArray(int len, int initalValue) {
  * Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n];
  * * Также заполнить элементы побочной диагонали
  */
+
+private static void zeroOne() {
+    int[] binMass = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+    printArray(binMass);
+    for (int i = 0; i < binMass.length; i++) binMass[i] = binMass[i] ^ 1;
+    printArray(binMass);
+}
+private static void sixx2() {
+    int[] sixMass = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+    printArray(sixMass);
+    for (int i = 0; i < sixMass.length; i++) if (sixMass[i] < 6) sixMass[i] *= 2;
+    printArray(sixMass);
+}
+
+private static void diagonal(int[][] in2dArray){
+    System.out.println();
+    print2dArray(in2dArray);
+    for (int i = 0; i < in2dArray.length; i++) {
+        for (int j = 0; j < in2dArray[0].length; j++) {
+            if (i == j) in2dArray[i][j] = 1; //главная диагональ
+            if (j == (in2dArray.length - i - 1)) in2dArray[i][j] = 1; //побочная диагональ
+        }
+    }
+    System.out.println();
+    print2dArray(in2dArray);
+}
+
 }
